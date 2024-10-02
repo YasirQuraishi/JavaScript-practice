76f5befb68c68a81c9b7916ef94d5588b4bdd252
Yaser Quraishi <yaserquraishi8@gmail.com>
2024-10-02 16:23:13 +0430

    revisiting sort of arrays js file added

---
+++ js/revisiting-sort.js
@@ -0,0 +1,66 @@
+
+ const books = [{
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'Changing My Mind',
+    authors: ['Zadie Smith'],
+    rating: 3.83,
+    genres: ['nonfiction', 'essays']
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42,
+    genres: ['fiction', 'graphic novel', 'fantasy']
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36,
+    genres: ['fiction', 'historical fiction']
+  },
+  {
+    title: 'The Name of the Wind',
+    authors: ['Patrick Rothfuss'],
+    rating: 4.54,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'The Overstory',
+    authors: ['Richard Powers'],
+    rating: 4.19,
+    genres: ['fiction', 'short stories']
+  },
+  {
+    title: 'The Way of Kings',
+    authors: ['Brandon Sanderson'],
+    rating: 4.65,
+    genres: ['fantasy', 'epic']
+  },
+  {
+    title: 'Lord of the flies',
+    authors: ['William Golding'],
+    rating: 3.67,
+    genres: ['fiction']
+  }
+];
+
+let sortOfBooks = books.sort((a,b)=> a.rating-b.rating);
+
+
+
+const bestScoresInMyClass= [30,32,40,40,39,35,39.5,37];
+
+let sortOfBestScores = bestScoresInMyClass.sort( (a,b)=> a-b);
+
+let deSortOfBestScores = bestScoresInMyClass.sort( (a,b)=> b-a);
